11d118929b73c908a6eccfdfbea289de59b1c3fe
MAYUMITAKEDA <mt1291092@gmail.com>
2023-05-09 16:02:12 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :profile
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
   end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 end
