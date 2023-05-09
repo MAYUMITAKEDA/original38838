19b80c2373297f1754f4b2297ea4f18803338614
MAYUMITAKEDA <mt1291092@gmail.com>
2023-05-09 20:20:32 +0900

    postsテーブルの作成

--- db/migrate/20230509110614_create_posts.rb
+++ db/migrate/20230509110614_create_posts.rb
@@ -1,7 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string     :title,           null: false
+      t.text       :text,            null: false
+      t.integer    :cost,            null: false
+      t.integer    :category_id,     null: false
+      t.integer    :budget_id,       null: false
+      t.integer    :opening_hour_id, null: false
+      t.references :user,            null: false, foreign_key: true
       t.timestamps
     end
   end
