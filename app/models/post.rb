531480982822cedeef6f61ac5818e942e3e550e2
MAYUMITAKEDA <mt1291092@gmail.com>
2023-05-15 15:28:39 +0900

    postモデルの実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,8 @@ class Post < ApplicationRecord
   has_many :post_tag_relations
   has_many :tags, through: :post_tag_relations
   has_many :likes
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
