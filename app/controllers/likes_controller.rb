99b036f2e6c62d1499de00171d9e2236cfbb09cd
MAYUMITAKEDA <mt1291092@gmail.com>
2023-05-29 17:35:27 +0900

    ビューの修正（いいね機能）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,13 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
     @like.save
-    render 'create.js.erb'
+    #render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    render 'destroy.js.erb'
+    #render 'destroy.js.erb'
   end
 
   def set_post
