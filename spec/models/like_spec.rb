50765d897d7d76a00caf005ca6fd16a5e5c3e153
MAYUMITAKEDA <mt1291092@gmail.com>
2023-05-30 18:23:11 +0900

    likeモデルの単体テスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  describe 'いいね機能' do
+    context 'いいねできるとき' do
+      it 'postとuserが紐づいていれば「いいね」することができる' do
+        expect(@like).to be_valid
+      end
+      it 'postの投稿userと同一userでもいいねすることができる' do
+        @like.user_id == @like.post.user_id
+        expect(@like).to be_valid
+      end
+    end
+
+    context 'いいねできないとき' do
+      it 'postが紐づいていなければ「いいね」できない' do
+        @like.post = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Post must exist")
+      end
+      it 'userが紐づいていなければ「いいね」できない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
