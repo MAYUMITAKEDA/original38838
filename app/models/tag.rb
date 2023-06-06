640e6a7b30c7d92f4ce5ee9da001f60d7f241887
MAYUMITAKEDA <mt1291092@gmail.com>
2023-06-06 22:51:30 +0900

    post_formモデルの再テスト

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :post_tag_relations
+  has_many :post_tag_relations, dependent: :destroy
   has_many :posts, through: :post_tag_relations
-  validates :tag_name, uniqueness: { case_sensitive: true }
 end
--- spec/factories/post_forms.rb
+++ spec/factories/post_forms.rb
@@ -3,10 +3,9 @@ FactoryBot.define do
     title           { Faker::Lorem.characters(number: 50) }
     text            { Faker::Lorem.characters(number: 2000) }
     address         { Faker::Lorem.characters(number: 200) }
-    budget          { Faker::Number.number(digits: 10) }
+    budget          { Faker::Number.within(range: 0..9_999_999) }
     category_id     { Faker::Number.between(from: 1, to: 11) }
     area_id         { Faker::Number.between(from: 1, to: 8) }
     opening_hour_id { Faker::Number.between(from: 1, to: 9) }
-    tag_name        { Faker::Lorem.characters(number: 10) }
   end
 end
