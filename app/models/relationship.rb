bbac1a7d374d7f0535be4efe98e65ef2a5e96f0f
MAYUMITAKEDA <mt1291092@gmail.com>
2023-06-06 17:03:49 +0900

    relationshipモデルのテスト

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,9 @@
 class Relationship < ApplicationRecord
   belongs_to :following, class_name: 'User'
   belongs_to :follower, class_name: 'User'
+
+  with_options presence: true do
+    validates :follower_id, presence: { message: "must exist"}
+    validates :following_id, presence: { message: "must exist"}
+  end 
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.create(:user)
+    @relationship = FactoryBot.build(:relationship, following: user1, follower: user2)
+  end
+
+  describe "ユーザーフォロー機能" do
+    context "フォローできるとき" do
+      it "フォロー申請する側の値(following_id)とフォローされる側の値（followed_id）が存在するとき" do
+        expect(@relationship).to be_valid
+      end
+    end
+
+    context "フォローできないとき" do
+      it "フォロー申請する側の値（following_id）がなければフォローできない" do
+        @relationship.following_id = nil
+        @relationship.valid?
+        expect(@relationship.errors.full_messages).to include('Following must exist')
+      end
+      it "フォローされる側の値（followed_is）がなければフォローできない" do
+        @relationship.follower_id = nil
+        @relationship.valid?
+        expect(@relationship.errors.full_messages).to include('Follower must exist')
+      end
+    end
+  end
 end
